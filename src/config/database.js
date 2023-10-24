6b321a742b625a4c99362441c2c7b62fda4e85eb
Eduardo dos Santos T <129340886+eudecap@users.noreply.github.com>
2023-10-24 17:51:29 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,10 @@
 module.exports = {
     dialect: 'postgres',
     host: 'localhost',
-    port: '3002',
     username: 'postgres',
     password: '0000',
     database: 'codeburger',
     define: {
-        timepamps: true,
         underscored: true,
         underscoredAll: true
     },
